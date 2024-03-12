83b7af4064bda8323b70a00ce3255cda92e0f03a
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2024-03-12 15:57:27 -0300

    Update exercicioGeradorHomemMulher..pl

--- exercicioGeradorHomemMulher..pl
+++ exercicioGeradorHomemMulher..pl
@@ -1,6 +1,47 @@
 % Student exercise profile
-start(A) :- write("Digite o valor de X:"), nl,
-	          read(X), write(X).
+homem(tom).
+homem(bob).
+homem(jim).
+mulher(pam).
+mulher(liz).
+mulher(ann).
+mulher(pat).
+gerador(pam, bob).
+gerador(tom, bob).
+gerador(tom, liz).
+gerador(bob, ann).
+gerador(bob, pat).
+gerador(pat, jim).
 
 % Your program goes here
-start(a).
+
+mulher(X).
+/*
+X = pam
+X = liz
+X = ann
+X = pat
+*/
+
+homem(X).
+/*
+X = tom
+X = bob
+X = jim
+*/
+
+gerador(X, pat).
+/*
+X = bob
+*/
+
+gerador(pat, X).
+/*
+X = jim
+*/
+
+gerador(bob, X).
+/*
+X = ann
+X = pat
+*/
