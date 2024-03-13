2fc81e8b77f3515aff7936d0cefae22f2c3c4dcc
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2024-03-12 22:13:24 -0300

    Update ExercicioCadeiaAlimentar.pl

--- ExercicioCadeiaAlimentar.pl
+++ ExercicioCadeiaAlimentar.pl
@@ -1,1 +1,25 @@
+% Student exercise profile
+animal(urso).
+animal(peixe).
+animal(peixinho).
+animal(lince).
+animal(raposa).
+animal(coelho).
+animal(veado).
+animal(guaxinim).
+planta(alga).
+planta(grama).
+come(urso, peixe).
+come(urso, raposa).
+come(urso, veado).
+come(urso, guaxinim).
+come(lince, veado).
+come(peixe, peixinho).
+come(peixinho, alga).
+come(guaxinim, peixe).
+come(raposa, coelho).
+come(coelho, grama).
+come(veado, grama).
+
+% Your program goes here
 
