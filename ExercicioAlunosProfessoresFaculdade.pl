19a5a009549147f521f26503ea0fe6c8b244b2b4
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2024-03-14 15:08:14 -0300

    Create ExercicioAlunosProfessoresFaculdade.pl

---
+++ ExercicioAlunosProfessoresFaculdade.pl
@@ -0,0 +1,20 @@
+%fatos
+aluno(joao, calculo).
+aluno(maria, calculo).
+aluno(joel, programacao).
+aluno(joel, estrutura).
+frequenta(joao, puc).
+frequenta(maria, puc).
+frequenta(joel, ufrj).
+professor(carlos, calculo).
+professor(ana, estrutura).
+professor(pedro, programacao).
+funcionario(carlos, puc).
+funcionario(ana, ufrj).
+funcionario(pedro, ufrj).
+
+%regras
+sao_alunos_do_professor(Y,X) :- aluno(Y, Z), professor(X, Z).
+alunos_da_faculdade(Y,X) :- frequenta(Y, X).
+professores_da_faculdade(Y,X) :- funcionario(Y,X).
+associados(Y,X) :- alunos_da_faculdade(Y,X); professores_da_faculdade(Y,X).
